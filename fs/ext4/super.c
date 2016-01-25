b9939bc467599cc9efca2337a24e06b16f29e9db
Jan Kara <jack@suse.cz>
2016-01-25 19:24:50 +0100

    quota: Add support for ->get_nextdqblk() for VFS quota
    Add infrastructure for supporting get_nextdqblk() callback for VFS
    quotas. Translate the operation into a callback to appropriate
    filesystem and consequently to quota format callback.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1117,6 +1117,7 @@ static const struct dquot_operations ext4_quota_operations = {
 	.alloc_dquot	= dquot_alloc,
 	.destroy_dquot	= dquot_destroy,
 	.get_projid	= ext4_get_projid,
+	.get_next_id	= dquot_get_next_id,
 };
 
 static const struct quotactl_ops ext4_qctl_operations = {
