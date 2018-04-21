1d99cbff408e52d1d25ab9d9b55f05b9a3731347
Jaegeuk Kim <jaegeuk@google.com>
2018-04-20 19:26:09 -0700

    dm-default-key, f2fs, ICE: support dm-default-key with f2fs/ICE
    This patch fixes assigning bi_crypt_key for moving data which was previously
    encrypted by f2fs.
    
    Note that, dm-default-key should not assign bi_crypt_key, if bi_crypt_skip is
    set.
    
    The issue sceanrios is:
    
    1. write data with user key by f2fs
      -  ENC(KU, IVU, DATA)
    2. log out user key
    3. read data #1 w/o user key from LBA #a
    4. dm-default-key assigns default key
      - DEC(KD, LBA#a, ENC(KU, IVU, DATA))
    5. write data #1 w/o user key into LBA #b
    6. dm-default-key assigns default key
      - ENC(KD, LBA#b, DEC(KD, LBA#a, ENC(KU, IVU, DATA)))
    7. Read DATA out with valid logged-in user key
      - DEC(KU, IVU, ENC(KD, LBA#b, DEC(KD, LBA#a, ENC(KU, IVU, DATA))))
    
    So, this patch introduces bi_crypt_skip to avoid 4. ~ 6 with right flow:
    1. write data with user key by f2fs
      -  ENC(KU, IVU, DATA)
    2. log out user key
    3. read data #1 w/o user key from LBA #a
    4. dm-default-key skip to assign default key
      - ENC(KU, IVU, DATA)
    5. write data #1 w/o user key into LBA #b
    6. dm-default-key skips to assign default key
      - ENC(KU, IVU, DATA)
    7. Try to read DATA with valid logged-in user key
      - DEC(KU, IVU, ENC(KU, IVU, DATA))
    
    Issue: 68721442
    Change-Id: Icefe85f608b7c3c84beb2bfa4267efd0f3787453
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Signed-off-by: Shivaprasad Hongal <shongal@codeaurora.org>
    [neersoni@codeaurora.corg: resolved merged conflicts, compilation issues.]
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -703,7 +703,9 @@ static inline void fscrypt_finalize_bounce_page(struct page **pagep)
 extern int fscrypt_using_hardware_encryption(const struct inode *inode);
 extern void fscrypt_set_ice_dun(const struct inode *inode,
 	struct bio *bio, u64 dun);
-extern bool fscrypt_mergeable_bio(struct bio *bio, u64 dun, bool bio_encrypted);
+extern bool fscrypt_mergeable_bio(struct bio *bio, u64 dun, bool bio_encrypted,
+		int bi_crypt_skip);
+extern void fscrypt_set_ice_skip(struct bio *bio, int bi_crypt_skip);
 #else
 static inline int fscrypt_using_hardware_encryption(const struct inode *inode)
 {
@@ -716,8 +718,12 @@ static inline void fscrypt_set_ice_dun(const struct inode *inode,
 	return;
 }
 
+static inline void fscrypt_set_ice_skip(struct bio *bio, int bi_crypt_skip)
+{
+}
+
 static inline bool fscrypt_mergeable_bio(struct bio *bio,
-	u64 dun, bool bio_encrypted)
+	u64 dun, bool bio_encrypted, int bi_crypt_skip)
 {
 	return true;
 }
