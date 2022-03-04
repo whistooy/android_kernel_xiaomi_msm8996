533e66695966d8f9a3a2077c6686f3ecf1ca22ff
Sultan Alsawaf <sultan@kerneltoast.com>
2022-03-03 21:36:03 -0800

    cpuidle: lpm-levels: Convert sync_lock to raw spin lock
    The CPU idle loop runs with IRQs disabled, so a raw spin lock must be used
    here.
    
    [B--B: adapted to 4.4 kernel from 4.14]
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -666,7 +666,7 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	if (cluster->min_child_level > child_idx)
 		return;
 
-	spin_lock(&cluster->sync_lock);
+	raw_spin_lock(&cluster->sync_lock);
 	cpumask_or(&cluster->num_children_in_sync, cpu,
 			&cluster->num_children_in_sync);
 
@@ -702,10 +702,10 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	cluster_prepare(cluster->parent, &cluster->num_children_in_sync, i,
 			from_idle, start_time);
 
-	spin_unlock(&cluster->sync_lock);
+	raw_spin_unlock(&cluster->sync_lock);
 	return;
 failed:
-	spin_unlock(&cluster->sync_lock);
+	raw_spin_unlock(&cluster->sync_lock);
 	if (IS_ENABLED(CONFIG_MSM_IDLE_STATS))
 		cluster->stats->sleep_time = 0;
 	return;
@@ -725,7 +725,7 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	if (cluster->min_child_level > child_idx)
 		return;
 
-	spin_lock(&cluster->sync_lock);
+	raw_spin_lock(&cluster->sync_lock);
 	last_level = cluster->default_level;
 	first_cpu = cpumask_equal(&cluster->num_children_in_sync,
 				&cluster->child_cpus);
@@ -782,7 +782,7 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	cluster_unprepare(cluster->parent, &cluster->child_cpus,
 			last_level, from_idle, end_time);
 unlock_return:
-	spin_unlock(&cluster->sync_lock);
+	raw_spin_unlock(&cluster->sync_lock);
 }
 
 static inline void cpu_prepare(struct lpm_cluster *cluster, int cpu_index,
@@ -848,7 +848,7 @@ int get_cluster_id(struct lpm_cluster *cluster, int *aff_lvl)
 	if (!cluster)
 		return 0;
 
-	spin_lock(&cluster->sync_lock);
+	raw_spin_lock(&cluster->sync_lock);
 
 	if (!cpumask_equal(&cluster->num_children_in_sync,
 				&cluster->child_cpus))
@@ -865,7 +865,7 @@ int get_cluster_id(struct lpm_cluster *cluster, int *aff_lvl)
 		(*aff_lvl)++;
 	}
 unlock_and_return:
-	spin_unlock(&cluster->sync_lock);
+	raw_spin_unlock(&cluster->sync_lock);
 	return state_id;
 }
 
@@ -1093,12 +1093,12 @@ static int cluster_cpuidle_register(struct lpm_cluster *cl)
 		p = per_cpu(cpu_cluster, cpu);
 		while (p) {
 			int j;
-			spin_lock(&p->sync_lock);
+			raw_spin_lock(&p->sync_lock);
 			cpumask_set_cpu(cpu, &p->num_children_in_sync);
 			for (j = 0; j < p->nlevels; j++)
 				cpumask_copy(&p->levels[j].num_cpu_votes,
 						&p->num_children_in_sync);
-			spin_unlock(&p->sync_lock);
+			raw_spin_unlock(&p->sync_lock);
 			p = p->parent;
 		}
 	}
@@ -1355,7 +1355,7 @@ enum msm_pm_l2_scm_flag lpm_cpu_pre_pc_cb(unsigned int cpu)
 	 * Assumes L2 only. What/How parameters gets passed into TZ will
 	 * determine how this function reports this info back in msm-pm.c
 	 */
-	spin_lock(&cluster->sync_lock);
+	raw_spin_lock(&cluster->sync_lock);
 
 	if (!cluster->lpm_dev) {
 		retflag = MSM_SCM_L2_OFF;
@@ -1380,6 +1380,6 @@ unlock_and_return:
 	trace_pre_pc_cb(retflag);
 	remote_spin_lock_rlock_id(&scm_handoff_lock,
 				  REMOTE_SPINLOCK_TID_START + cpu);
-	spin_unlock(&cluster->sync_lock);
+	raw_spin_unlock(&cluster->sync_lock);
 	return retflag;
 }
