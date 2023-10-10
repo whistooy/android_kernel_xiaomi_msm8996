e21a4fa72b6f49fe60bff473bac812b76fc430af
Marco Zanin <mrczn.bb@gmail.com>
2023-10-11 00:25:09 +0200

    fs/crypto-f2fs: remove duplicate defines
    Op helpers has been backported, we don't need these workarounds anymore
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- fs/crypto/fscrypt_private.h
+++ fs/crypto/fscrypt_private.h
@@ -86,17 +86,6 @@ typedef enum {
 
 #define FS_CTX_REQUIRES_FREE_ENCRYPT_FL		0x00000001
 
-/* bio stuffs */
-#define REQ_OP_READ	READ
-#define REQ_OP_WRITE	WRITE
-#define bio_op(bio)	((bio)->bi_rw & 1)
-
-static inline void bio_set_op_attrs(struct bio *bio, unsigned op,
-		unsigned op_flags)
-{
-	bio->bi_rw = op | op_flags;
-}
-
 static inline bool fscrypt_valid_enc_modes(u32 contents_mode,
 					   u32 filenames_mode)
 {
