c46bfd581ec41fec9bb6dc0bd8da4eb1eae0b399
Marco Zanin <mrczn.bb@gmail.com>
2023-11-11 23:21:15 +0100

    Block: fix "defer timeouts to a workqueue" backport
    Commit 5fb835b9561f30188a3bfb5e66295c13348752b9 "block: defer timeouts to a workqueue"
    has been bacported only partially on 4.4 kernel, but then a race condition
    between blk_cleanup_queue() and timeout handling has been discovered, and
    never fixed here: this commit complete the backport and include the subsequent
    fix (commit e5386fca7e04b281e59a719a76243d83e3c2d63a "block: Fix a race between blk_cleanup_queue() and timeout handling")
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -294,6 +294,7 @@ EXPORT_SYMBOL(blk_stop_queue);
 void blk_sync_queue(struct request_queue *q)
 {
 	del_timer_sync(&q->timeout);
+	cancel_work_sync(&q->timeout_work);
 
 	if (q->mq_ops) {
 		struct blk_mq_hw_ctx *hctx;
@@ -688,6 +689,13 @@ static void blk_queue_usage_counter_release(struct percpu_ref *ref)
 	wake_up_all(&q->mq_freeze_wq);
 }
 
+static void blk_rq_timed_out_timer(unsigned long data)
+{
+	struct request_queue *q = (struct request_queue *)data;
+
+	kblockd_schedule_work(&q->timeout_work);
+}
+
 struct request_queue *blk_alloc_queue_node(gfp_t gfp_mask, int node_id)
 {
 	struct request_queue *q;
@@ -718,6 +726,7 @@ struct request_queue *blk_alloc_queue_node(gfp_t gfp_mask, int node_id)
 	setup_timer(&q->backing_dev_info->laptop_mode_wb_timer,
 		    laptop_mode_timer_fn, (unsigned long) q);
 	setup_timer(&q->timeout, blk_rq_timed_out_timer, (unsigned long) q);
+	INIT_WORK(&q->timeout_work, NULL);
 	INIT_LIST_HEAD(&q->queue_head);
 	INIT_LIST_HEAD(&q->timeout_list);
 	INIT_LIST_HEAD(&q->icq_list);
@@ -851,6 +860,7 @@ blk_init_allocated_queue(struct request_queue *q, request_fn_proc *rfn,
 	if (blk_init_rl(&q->root_rl, q, GFP_KERNEL))
 		goto fail;
 
+	INIT_WORK(&q->timeout_work, blk_timeout_work);
 	q->request_fn		= rfn;
 	q->prep_rq_fn		= NULL;
 	q->unprep_rq_fn		= NULL;
